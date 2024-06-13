ec926fc8bcd204a79f47ac5350cedd6ba5f51772
aleja4arboleda <124401203+aleja4arboleda@users.noreply.github.com>
2024-06-12 20:23:14 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+from analysis.analisisAire import * 
+from analysis.analisisArboles import *
+from analysis.analisisEmision import * 
+from analysis.analisisResiduos import * 
+from analysis.analisisRuido import * 
+
